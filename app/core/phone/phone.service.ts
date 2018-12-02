25a00d6739b6a372bd90492ab800b8ed05489268
Santosh Ugale <santoshugale1991@gmail.com>
2018-12-02 14:03:14 +0530

    phone.service.js converted to phone.service.ts

--- app/core/phone/phone.service.ts
+++ app/core/phone/phone.service.ts
@@ -3,11 +3,11 @@
 angular.
   module('core.phone').
   factory('Phone', ['$resource',
-    function($resource) {
+    function ($resource: angular.resource.IResourceService) {
       return $resource('phones/:phoneId.json', {}, {
         query: {
           method: 'GET',
-          params: {phoneId: 'phones'},
+          params: { phoneId: 'phones' },
           isArray: true
         }
       });
